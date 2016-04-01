a5db62dbf4b250167b37fda2206391cb1ea92e5d
Connor Smith <aggiesuperfan@gmail.com>
2016-03-31 22:51:59 -0500

    Added files via upload

---
+++ game.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+$link = new mysqli("localhost","root","","game");
+if ($link->connect_errno) {
+    printf("Connect failed: %s\n", $link->connect_error);
+    exit();
+}
+if(isset($_SESSION['user'])){
+	$email = $_SESSION["user"];
+	$password = $_SESSION[$email];
+	$result = $link->query("SELECT password FROM users where email='$email'");
+	$row = $result->fetch_assoc();
+	if($password != $row["password"]){
+		header('Location: index.php');
+	}
+}else{
+	header('Location: index.php');
+}
+
+$email = $_SESSION["user"];
+$fullname="";
+$result = $link->query("SELECT first_name, last_name, id FROM users where email='$email'");
+$row = $result->fetch_assoc();
+$first = $row['first_name'];
+$last = $row['last_name'];
+$fullname = $first." ".$last;
+$userID = $row['id'];
+
+if(isset($_REQUEST["action"]))
+	$action = $_REQUEST["action"];
+else
+	$action = "none";
+
+?>
+
+<html>
+</html>
