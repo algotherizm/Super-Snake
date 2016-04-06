7f79d53829972c63d326136deb78119133e82add
kmh12c <kaylaholcomb@Kaylas-MacBook-Pro.local>
2016-04-06 12:38:07 -0500

    snake from paper

--- main.php
+++ main.php
@@ -39,7 +39,51 @@ else
 	    <title>Super-Snake</title>
 	    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
 	    <link href="../Super-Snake/css/main.css" rel="stylesheet">
-	    <script>
+	    <link rel="stylesheet" href="../js/paperjs-v0/css/style.css">
+		<script type="text/javascript" src="../../dist/paper-full.js"></script>
+		<script type="text/paperscript" canvas="canvas">
+
+			// Adapted from the following Processing example:
+	        // http://processing.org/learning/topics/follow3.html
+
+	        // The amount of points in the path:
+	        var points = 25;
+
+	        // The distance between the points:
+	        var length = 35;
+
+	        var path = new Path({
+	            strokeColor: '#E4141B',
+	            strokeWidth: 20,
+	            strokeCap: 'round'
+	        });
+
+	        var start = view.center / [10, 1];
+	        for (var i = 0; i < points; i++)
+	            path.add(start + new Point(i * length, 0));
+
+	        function onMouseMove(event) {
+	            path.firstSegment.point = event.point;
+	            for (var i = 0; i < points - 1; i++) {
+	                var segment = path.segments[i];
+	                var nextSegment = segment.next;
+	                var vector = segment.point - nextSegment.point;
+	                vector.length = length;
+	                nextSegment.point = segment.point - vector;
+	            }
+	            path.smooth();
+	        }
+
+	        function onMouseDown(event) {
+	            path.fullySelected = true;
+	            path.strokeColor = '#e08285';
+	        }
+
+	        function onMouseUp(event) {
+	            path.fullySelected = false;
+	            path.strokeColor = '#e4141b';
+	        }
+
 	    </script>
 	</head>
 	<body>
@@ -53,10 +97,13 @@ else
 	            </nav>
 	            <a href="main.php"><h1 class="text-muted"><span class="glyphicon glyphicon-globe"></span> Super-Snake</h1></a>
 	        </div>
+			    
+			<canvas id="canvas" resize></canvas>
+
 	        <footer class="footer">
 	            <p>&copy; Connor Smith and Kayla Holcomb 2016</p>
 	        </footer>
 	    </div>
-	    <canvas id="myCanvas" style="border:1px solid #000000;"/>
-	</body>
+<!-- 	    <canvas id="myCanvas" style="border:1px solid #000000;"/>
+ -->	</body>
 </html>
